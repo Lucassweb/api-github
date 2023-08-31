075bb3ad1205cc6af90976d5d351498559a5c890
Lucas <lucas.26.04@hotmail.com>
2023-08-30 22:05:12 -0300

    Trabalhando com as typagens, fazendo o data receber esses valores

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,19 +3,31 @@ import Search from "../components/Search";
 import { UserProps } from "../types/user";
 
 const Home = () => {
-  const [user, setUser] = useState<UserProps | null>(null);
   //Estou inicializando null, para saber quando tem usuario e quando nao tem
+  const [user, setUser] = useState<UserProps | null>(null);
 
   //Buscando API
   const loadUser = async (userName: string) => {
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
-    console.log(data);
+
+    const { avatar_url, login, location, followers_url, following_url } = data;
+
+    const userData: UserProps = {
+      avatar_url,
+      login,
+      location,
+      followers_url,
+      following_url,
+    };
+    setUser(userData); //Recebendo as typagens
   };
 
   return (
     <div>
       <Search loadUser={loadUser} />
+      {user && <p>{user.login}</p>}{" "}
+      {/*  Fazendo verificação se usuário existi */}
     </div>
   );
 };
