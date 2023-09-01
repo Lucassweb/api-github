a9ab69e7be4849c6fb294f00e31a34b58a925784
Lucas <lucas.26.04@hotmail.com>
2023-09-01 15:56:36 -0300

    Estilizamos o component Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -4,19 +4,25 @@ type SearchProps = {
 
 import { BsSearch } from "react-icons/bs";
 import { useState } from "react";
+import classes from './Search.module.css'
 
 const Search = ({ loadUser }: SearchProps) => {
   const [userName, setUserName] = useState("");
 
+  // const handleKeyDown = (e) => {
+    
+  // }
+
   return (
-    <div>
+    <div className={classes.search}>
       <h2>Busque por um usuário:</h2>
       <p>Conheça seus melhores repositórios</p>
-      <div>
+      <div className={classes.search_container}>
         <input
           type="text"
           placeholder="Digite o nome do usuário"
           onChange={(e) => setUserName(e.target.value)}//Quando usuario for digitar no input
+          // onKeyDown={handleKeyDown}
         />
         <button onClick={() => loadUser(userName)}>
           {" "}
