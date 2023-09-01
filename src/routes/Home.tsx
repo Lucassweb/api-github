814d438e7c59227e5a3e5373102137b9659764a0
Lucas <lucas.26.04@hotmail.com>
2023-09-01 17:14:51 -0300

    Criamos um components Error.tsx

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -3,15 +3,25 @@ import Search from "../components/Search";
 import { UserProps } from "../types/user";
 
 import User from "../components/User";
+import Error from "../components/Error";
 
 const Home = () => {
   //Estou inicializando null, para saber quando tem usuario e quando nao tem
   const [user, setUser] = useState<UserProps | null>(null);
+  const [error, setError] = useState(false);
 
   //Buscando API
   const loadUser = async (userName: string) => {
+    setError(false);
+    setUser(null);
+
     const res = await fetch(`https://api.github.com/users/${userName}`);
     const data = await res.json();
+    
+    if (res.status === 404) {
+      setError(true);
+      return;
+    }
 
     const { avatar_url, login, location, followers, following } = data;
 
@@ -31,6 +41,7 @@ const Home = () => {
       {user && <p>{user.login}</p>}{" "}
       {/*  Fazendo verificação se usuário existi */}
       {user && <User {...user} />}
+      {error && <Error/>}
     </div>
   );
 };
